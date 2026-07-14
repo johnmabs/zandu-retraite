13f6d5512e50b73c64d9f7e3203f0a06512a3ecd
John Mabiala <johnmabiala@hotmail.com>
2026-07-14 11:16:00 +0100

    feat: add public registration flow (controller + templates)
    Open access_control on /espace-client/inscription (PUBLIC_ACCESS),
    otherwise covered by the ROLE_MEMBER rule on the rest of the
    member area.

---
+++ src/Controller/Member/RegistrationController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Member;
+
+use App\Entity\Member;
+use App\Form\RegistrationType;
+use App\Service\MemberRegistrationService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/espace-client/inscription', name: 'member_registration')]
+    public function register(Request $request, MemberRegistrationService $registrationService): Response
+    {
+        $member = new Member();
+        $form = $this->createForm(RegistrationType::class, $member);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $registrationService->register($member, $form->get('pin')->getData());
+
+            $this->addFlash('success', sprintf(
+                'Inscription enregistrée sous le numéro %s. Elle sera examinée par un administrateur.',
+                $member->getMemberNumber(),
+            ));
+
+            return $this->redirectToRoute('member_registration_success', ['memberNumber' => $member->getMemberNumber()]);
+        }
+
+        return $this->render('member/registration.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/espace-client/inscription/succes/{memberNumber}', name: 'member_registration_success')]
+    public function success(string $memberNumber): Response
+    {
+        return $this->render('member/registration_success.html.twig', [
+            'memberNumber' => $memberNumber,
+        ]);
+    }
+}
