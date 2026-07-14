5258175ce99a42d0cc350ca96ebbc15bf3b5ae98
John Mabiala <johnmabiala@hotmail.com>
2026-07-14 11:36:46 +0100

    feat(repository): add SubSectorRepository::findBySector

--- src/Repository/SubSectorRepository.php
+++ src/Repository/SubSectorRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Sector;
 use App\Entity\SubSector;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -14,5 +15,15 @@ class SubSectorRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, SubSector::class);
+    }
+
+    public function findBySector(Sector $sector): array
+    {
+        return $this->createQueryBuilder('sub')
+            ->andWhere('sub.sector = :sector')
+            ->setParameter('sector', $sector)
+            ->orderBy('sub.name', 'ASC')
+            ->getQuery()
+            ->getResult();
     }
 }
